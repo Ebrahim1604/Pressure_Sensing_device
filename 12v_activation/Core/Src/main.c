dc2f2958e2e30d504b25eaf4165fed5b88db41dd
ebrahim1604 <rupawalaebrahim@gmail.com>
2024-02-14 13:44:19 +0530

    12v activation code pull down added

--- 12v_activation/Core/Src/main.c
+++ 12v_activation/Core/Src/main.c
@@ -275,7 +275,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : PA4 */
   GPIO_InitStruct.Pin = GPIO_PIN_4;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : LD2_Pin */
